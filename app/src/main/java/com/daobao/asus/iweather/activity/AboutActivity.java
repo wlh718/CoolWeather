3353d6986650f1cc610d1b1723e1ee0c649d790f
angelaapple <1824633808@qq.com>
2018-12-27 22:04:27 +0800

    four commit

--- app/src/main/java/com/daobao/asus/iweather/activity/AboutActivity.java
+++ app/src/main/java/com/daobao/asus/iweather/activity/AboutActivity.java
@@ -18,7 +18,6 @@ import com.daobao.asus.iweather.R;
 
 public class AboutActivity extends AppCompatActivity implements View.OnClickListener{
     private Toolbar mToolbar;
-    private Button mCodeButton;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +27,6 @@ public class AboutActivity extends AppCompatActivity implements View.OnClickList
 
     private void initView() {
         mToolbar = findViewById(R.id.about_toolbar);
-        mCodeButton = findViewById(R.id.about_bt_code);
         setSupportActionBar(mToolbar);
         mToolbar.setNavigationIcon(R.mipmap.ic_arrow_back_white);
         mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -37,20 +35,10 @@ public class AboutActivity extends AppCompatActivity implements View.OnClickList
                 AboutActivity.this.onBackPressed();
             }
         });
-        mCodeButton.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId())
-        {
-            case R.id.about_bt_code:
-                Intent intent =new Intent();//创建Intent对象
-                intent.setAction(Intent.ACTION_VIEW);//为Intent设置动作
-                String data = "https://github.com/1006112544/IWeather";//获取编辑框里面的文本内容
-                intent.setData(Uri.parse(data));//为Intent设置数据
-                startActivity(intent);//将Intent传递给Activity
-                break;
-        }
     }
 }
